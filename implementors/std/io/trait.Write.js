01aba441576742d846c3551425fd1e7c77b641a8
Leif Walsh <leif.walsh@gmail.com>
2015-03-25 04:58:02 +0000

    rebuild pages at 7edb13c

---
+++ implementors/std/io/trait.Write.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['irc'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Write.html' title='std::io::Write'>Write</a> for <a class='struct' href='irc/event_stream/struct.EventStream.html' title='irc::event_stream::EventStream'>EventStream</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ irc/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Client","The top-level IRC client."]],"mod":[["event_stream",""],["protocol",""]]});
