b451faa1f951ae426f32bb9a23cecddfbd2a4852
Leif Walsh <leif.walsh@gmail.com>
2015-04-07 00:10:42 -0400

    added command line options

--- src/bin/hiphopabotamus.rs
+++ src/bin/hiphopabotamus.rs
@@ -4,10 +4,14 @@ use irc::event_stream::{Action, HandlerAction, Response};
 use irc::protocol;
 use regex::Regex;
 use rand::{thread_rng, Rng};
+use std::ascii::AsciiExt;
 use std::collections::{hash_map, HashMap};
+use std::env;
+use std::str::FromStr;
 use std::sync::{Arc, Mutex};
 
 extern crate env_logger;
+extern crate getopts;
 extern crate irc;
 extern crate rand;
 extern crate regex;
@@ -18,18 +22,30 @@ extern crate log;
 fn main() {
     env_logger::init().unwrap();
 
-    // let addr = ("irc.foonetic.net", 6667);
-    // let nick = "hiphopabotamus";
-    // let channels = ["#botwerks"];
+    let mut opts = getopts::Options::new();
+    opts.reqopt("", "host", "irc server hostname", "HOSTNAME");
+    opts.optopt("", "port", "irc server port", "PORT");
+    opts.reqopt("n", "nick", "nickname", "NICK");
+    opts.optmulti("c", "chan", "channels to join", "CHAN");
 
-    let addr = ("irc.freenode.net", 6667);
-    let nick = "hiphopabotamus";
-    let channels = ["#raptracks"];
+    let args: Vec<String> = env::args().collect();
+    let matches = opts.parse(&args[1..]).unwrap();
+
+    let host = matches.opt_str("host").expect("must provide --host");
+    let mut port = 6667;
+    if matches.opt_present("port") {
+        port = u16::from_str(&matches.opt_str("port").expect("must provide argument to --port")).unwrap();
+    }
+
+    let addr = (host.as_ref(), port);
+    let nick = matches.opt_str("nick").expect("must provide --nick");
+    let channels = matches.opt_strs("chan");
 
     let knowledge_mutex: Arc<Mutex<HashMap<String, Vec<String>>>> = Arc::new(Mutex::new(HashMap::new()));
 
+    let choice_nick = nick.clone();
     let choice_handler = box move |line: &str| {
-        if let Some(pm) = protocol::Privmsg::parse(line).and_then(|pm| pm.targeted_msg(nick)) {
+        if let Some(pm) = protocol::Privmsg::parse(line).and_then(|pm| pm.targeted_msg(&choice_nick)) {
             let choices: Vec<_> = Regex::new(r"\s+or\s+").unwrap()
                 .split(pm.msg)
                 .map(|s| s.trim())
@@ -37,7 +53,7 @@ fn main() {
                 .collect();
             if choices.len() > 1 {
                 let mut rng = thread_rng();
-                if let Some(reply_to) = pm.reply_target(nick) {
+                if let Some(reply_to) = pm.reply_target(&choice_nick) {
                     return Response::respond(protocol::Privmsg::new(reply_to, rng.choose(&choices).unwrap()).format());
                 }
             }
@@ -46,8 +62,9 @@ fn main() {
     };
 
     let knowledge_mutex_1 = knowledge_mutex.clone();
+    let learning_nick = nick.clone();
     let learning_handler = box move |line: &str| {
-        if let Some(pm) = protocol::Privmsg::parse(line).and_then(|pm| pm.targeted_msg(nick)) {
+        if let Some(pm) = protocol::Privmsg::parse(line).and_then(|pm| pm.targeted_msg(&learning_nick)) {
             let assignment: Vec<_> = Regex::new(r"\s+is\s+").unwrap()
                 .splitn(pm.msg, 2)
                 .map(|s| s.trim())
@@ -69,9 +86,10 @@ fn main() {
     };
 
     let knowledge_mutex_2 = knowledge_mutex.clone();
+    let info_nick = nick.clone();
     let info_handler = box move |line: &str| {
         if let Some(pm) = protocol::Privmsg::parse(line) {
-            if let Some(reply_to) = pm.reply_target(nick) {
+            if let Some(reply_to) = pm.reply_target(&info_nick) {
                 let knowledge = knowledge_mutex_2.lock().unwrap();
                 if let Some(choices) = knowledge.get(pm.msg) {
                     let mut rng = thread_rng();
@@ -82,10 +100,11 @@ fn main() {
         Response::nothing()
     };
 
+    let echo_nick = nick.clone();
     let echo_handler = box move |line: &str| {
-        if let Some(pm) = protocol::Privmsg::parse(line).and_then(|pm| pm.targeted_msg(nick)) {
+        if let Some(pm) = protocol::Privmsg::parse(line).and_then(|pm| pm.targeted_msg(&echo_nick)) {
             if Regex::new(r"^[Hh]i$").ok().expect("bad regex").is_match(pm.msg) {
-                if let Some(reply_to) = pm.reply_target(nick) {
+                if let Some(reply_to) = pm.reply_target(&echo_nick) {
                     if let Some(protocol::Source::User(ref user_info)) = pm.src {
                         return Response::respond(protocol::Privmsg::new(reply_to, &format!("Hi, {}!", user_info.nick)).format());
                     }
@@ -98,7 +117,7 @@ fn main() {
     };
 
     let (mut client, join_handle) = irc::Client::connect(
-        &addr, nick, &channels, "leifw_rustbot", "leifw's rust robot")
+        &addr, &nick, &channels.iter().map(|s| s.as_ref()).collect::<Vec<&str>>(), "leifw_rustbot", "leifw's rust robot")
         .ok().expect(&format!("Error connecting to {:?}.", addr));
 
     client.add_handler(choice_handler);
