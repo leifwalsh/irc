e63947b234bb03243bb2a9f960fed8d4abed2897
Leif Walsh <leif.walsh@gmail.com>
2015-04-07 04:31:30 +0000

    rebuild pages at 6dc0d43

--- irc/protocol/sidebar-items.js
+++ irc/protocol/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Privmsg",""],["UserInfo",""]],"enum":[["Dest",""],["Source",""]],"fn":[["join",""],["login",""],["pong_handler",""],["timeout_handler",""]]});
+initSidebarItems({"struct":[["Privmsg",""],["UserInfo",""]],"fn":[["ctcp_action",""],["join",""],["login",""],["pong_handler",""],["timeout_handler",""]],"enum":[["Dest",""],["Source",""]]});
